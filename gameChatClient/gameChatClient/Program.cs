a0d35f57a42fa95a34ca2a0d3e55f9eb8215caf9
gogo392539 <gogo392539@gmail.com>
2017-05-15 19:17:56 +0900

    ...

---
+++ gameChatClient/gameChatClient/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+
+namespace gameChatClient
+{
+    public class CONSTANTNUMBER
+    {
+        public const int CLIENT_MAX = 1;
+        public const int DEFAULT_MESSAGA = 1024;
+        public const int DEFAULT_PORT = 30000;
+    }
+
+    public class IPADDRESS
+    {
+        public const string GO_SERVER = "192.168.63.41";
+        public const string HY_SERVER = "192.168.63.47";
+        public const string DH_SERVER = "172.30.32.244";
+        public const string LOCAL_SERVER = "127.0.0.1";
+    }
+
+    public class ByteAndString
+    {
+        // 바이트 배열을 String으로 변환 
+        public static string ByteToString(byte[] strByte) {
+            string str = Encoding.Default.GetString(strByte);
+            return str;
+        }
+        // String을 바이트 배열로 변환 
+        public static byte[] StringToByte(string str) {
+            byte[] StrByte = Encoding.UTF8.GetBytes(str);
+            return StrByte;
+        }
+
+       
+    }
+
+    public class ChatClient
+    {
+        private int myID;
+        private Socket clientChatSock;
+        private IPEndPoint chatIpep;
+
+        private ThreadStart sendThreadStart;
+        private ThreadStart recvThreadStart;
+        private Thread sendThread;
+        private Thread recvThread;
+
+        public ChatClient()
+        {
+            myID = -1;
+        }
+
+        public void serverInit()
+        {
+            chatIpep = new IPEndPoint(IPAddress.Parse(IPADDRESS.LOCAL_SERVER), 30000);
+            clientChatSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientChatSock.Connect(chatIpep);
+        }
+
+        public void recvClientID()
+        {
+            Byte[] tempData = new Byte[sizeof(int)];
+            clientChatSock.Receive(tempData);
+            myID = BitConverter.ToInt32(tempData, 0);           //ID setting
+        }
+
+        public void ChatThreadStart()
+        {
+            sendThreadStart = new ThreadStart(this.ChatSendThread);
+            sendThread = new Thread(sendThreadStart);
+            sendThread.Start();
+
+            recvThreadStart = new ThreadStart(this.ChatRecvThread);
+            recvThread = new Thread(recvThreadStart);
+            recvThread.Start();
+        }
+
+        public void ChatSendThread()
+        {
+            byte[] sendData = new byte[CONSTANTNUMBER.DEFAULT_MESSAGA];
+            String str;
+
+            while (true)
+            {
+                //Console.Write("입력 : ");
+                str = Console.ReadLine();
+                Console.WriteLine(str); //
+                sendData = ByteAndString.StringToByte(str);
+                clientChatSock.Send(sendData);
+            }
+        }
+
+        public void ChatRecvThread()
+        {
+            byte[] recvData = new byte[CONSTANTNUMBER.DEFAULT_MESSAGA];
+            string str;
+
+            while (true)
+            {
+                clientChatSock.Receive(recvData);
+                str = ByteAndString.ByteToString(recvData);
+                Console.WriteLine(str);
+            }
+        }
+
+        public void ThreadJoin()
+        {
+            sendThread.Join();
+            recvThread.Join();
+        }
+
+        public void clientClosed()
+        {
+            clientChatSock.Close();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chatting Start");
+            ChatClient client = new ChatClient();
+            client.serverInit();
+            client.recvClientID();
+            client.ChatThreadStart();
+            client.ThreadJoin();
+            client.clientClosed();
+        }
+    }
+}
--- gameChatServer/gameChatServer/chattingServer.cpp
+++ gameChatServer/gameChatServer/chattingServer.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <thread>
 #include <WinSock2.h>
+#include <string>
 
-#define CLIENT_MAX 5
+#define CLIENT_MAX 1
 #define DEFAULT_PORT 30000
+#define DEFAULT_MESSAGE 1024
 
 struct clientChatState {
 	int id;
 	SOCKET clientSock;
 };
 
-int chat_client();
+int chat_client(clientChatState* clients, int myID);
+void error_handling(char* message);
 
 using namespace std;
 
 int main(void) {
 	
 	SOCKET serverSock;
-	clientChatState clients;
+	clientChatState clients[CLIENT_MAX];
 
 	SOCKADDR_IN serverAddr;
 	SOCKADDR_IN clientAddr;
@@ -30,17 +33,82 @@ int main(void) {
 		error_handling("WSAStartup() error");
 	}
 
+	serverSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (serverSock == SOCKET_ERROR) {
+		error_handling("socket() error");
+	}
 
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(DEFAULT_PORT);
 
+	if (::bind(serverSock, (sockaddr*)&serverAddr, sizeof(serverAddr))) {
+		error_handling("bind() error");
+	}
+	if (listen(serverSock, 5)) {
+		error_handling("listen() error");
+	}
+	cout << "chatting Server Start" << endl;
+	while (connectNum < CLIENT_MAX) {
+		clientAddrsz = sizeof(clientAddr);
+		clients[connectNum].clientSock = accept(serverSock, (sockaddr*)&clientAddr, &clientAddrsz);
+		if (clients[connectNum].clientSock == SOCKET_ERROR) {
+			error_handling("accept() error");
+		}
+		clients[connectNum].id = connectNum;
 
+		send(clients[connectNum].clientSock, (char*)&clients[connectNum].id, sizeof(int), 0);			//id РќДо
+
+		client_threads[connectNum] = thread(chat_client, clients, connectNum);
+		connectNum++;
+	}
+
+	for (int i = 0; i < CLIENT_MAX; i++) {
+		client_threads[i].join();
+	}
+
+	closesocket(serverSock);
+	for (int i = 0; i < CLIENT_MAX; i++) {
+		closesocket(clients[i].clientSock);
+	}
 
 
 	WSACleanup();
 	return 0;
 }
 
-int chat_client() {
+int chat_client(clientChatState* clients, int ID) {
+	int myID = ID;
+	clientChatState* Clients = clients;
 
+	string message = "";
+	char tempMessage[DEFAULT_MESSAGE] = "";
+
+	while (1) {
+		memset(tempMessage, 0, DEFAULT_MESSAGE);
+
+		int recvResult = recv(clients[myID].clientSock, tempMessage, DEFAULT_MESSAGE, 0);
+		if (recvResult != SOCKET_ERROR) {
+			if (strcmp("", tempMessage)) {
+				message = "Client #" + std::to_string(clients[myID].id) + ": " + tempMessage;
+			}
+			cout << message << endl;
+
+			for (int i = 0; i < CLIENT_MAX; i++) {
+				if (clients[i].clientSock != INVALID_SOCKET) 
+					if (clients[myID].id != i) 
+						int sendResult = send(clients[i].clientSock, message.c_str(), strlen(message.c_str()), 0);
+			}
+		}
+		else {
+			message = "Client #" + std::to_string(clients[myID].id) + " Disconnected";
+			clients[myID].clientSock = INVALID_SOCKET;
+			break;
+		}
+	}
+
+	return 0;
 }
 
 void error_handling(char* message) {
